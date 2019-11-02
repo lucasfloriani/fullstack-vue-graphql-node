3a430523cb3aae411b512bb42ea07e61030121b6
lucasfloriani <lucas.floriane@gmail.com>
2019-11-02 03:00:02 -0300

    Code from aula 6

---
+++ 6-aula/client/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div class="text-center" id="slogan">
+      <h1>NameGator</h1>
+      <br>
+      <h6 class="text-secondary">Gerador de nomes utilizando Vue.js, GraphQL e Node</h6>
+    </div>
+    <div id="main">
+			<DomainList />
+    </div>
+  </div>
+</template>
+
+<script>
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
+import DomainList from "./components/DomainList";
+
+export default {
+	name: "app",
+	components: {
+		DomainList,
+	}
+};
+</script>
+
+<style>
+#slogan {
+  margin-top: 30px;
+  margin-bottom: 30px;
+}
+#main {
+  background-color: #f1f1f1;
+  padding-bottom: 30px;
+  padding-top: 30px;
+}
+</style>
---
+++ 6-aula/client/src/components/DomainList.vue
@@ -0,0 +1,154 @@
+<template>
+  <div>
+    <div id="main">
+      <div class="container">
+        <div class="row">
+          <div class="col-md">
+						<AppItemList
+							title="Prefixos"
+							type="prefix"
+							v-bind:items="items.prefix"
+							v-on:addItem="addItem"
+							v-on:deleteItem="deleteItem"
+						/>
+          </div>
+          <div class="col-md">
+						<AppItemList
+							title="Sufixos"
+							type="sufix"
+							v-bind:items="items.sufix"
+							v-on:addItem="addItem"
+							v-on:deleteItem="deleteItem"
+						/>
+          </div>
+        </div>
+        <br>
+        <h5>Domínios <span class="badge badge-info">{{ domains.length }}</span></h5>
+        <div class="card">
+          <div class="card-body">
+            <ul class="list-group">
+              <li class="list-group-item" v-for="domain in domains" v-bind:key="domain.name">
+								<div class="row">
+									<div class="col-md">
+										{{ domain.name }}
+									</div>
+									<div class="col-md text-right">
+										<a class="btn btn-info" v-bind:href="domain.checkout" target="_blank">
+											<span class="fa fa-shopping-cart"></span>
+										</a>
+									</div>
+								</div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios/dist/axios";
+import AppItemList from "./AppItemList";
+
+export default {
+	name: "app",
+	components: {
+		AppItemList,
+	},
+	data() {
+		return {
+			items: {
+				prefix: [],
+				sufix: [],
+			}
+		};
+	},
+	methods: {
+		addItem(item) {
+			axios({
+				url: "http://localhost:4000",
+				method: "post",
+				data: {
+					query: `
+						mutation ($item: ItemInput) {
+							newItem: saveItem(item: $item) {
+								id
+								type
+								description
+							}
+						}
+					`,
+					variables: {
+						item
+					}
+				}
+			}).then(response => {
+				const query = response.data;
+				const newItem = query.data.newItem;
+				this.items[item.type].push(newItem);
+			});
+		},
+		deleteItem(item) {
+			axios({
+				url: "http://localhost:4000",
+				method: "post",
+				data: {
+					query: `
+						mutation ($id: Int) {
+							deleted: deleteItem(id: $id)
+						}
+					`,
+					variables: {
+						id: item.id,
+					}
+				}
+			}).then(() => this.getItems(item.type));
+		},
+		getItems(type) {
+			axios({
+				url: "http://localhost:4000",
+				method: "post",
+				data: {
+					query: `
+						query ($type: String){
+							items (type: $type) {
+								id
+								type
+								description
+							}
+						}
+					`,
+					variables: {
+						type,
+					}
+				}
+			}).then(response => {
+				const query = response.data;
+				this.items[type] = query.data.items;
+			});
+		},
+	},
+	computed: {
+		domains() {
+			const domains = [];
+			for (const prefix of this.items.prefix) {
+				for (const sufix of this.items.sufix) {
+					const name = prefix.description + sufix.description;
+					const url = name.toLowerCase();
+					const checkout = `https://checkout.hostgator.com.br/?a=add&sld=${url}&tld=.com.br`;
+					domains.push({ name, checkout });
+				}
+			}
+			return domains;
+		}
+	},
+	created() {
+		this.getItems("prefix");
+		this.getItems("sufix");
+	}
+};
+</script>
+
+<style>
+</style>
